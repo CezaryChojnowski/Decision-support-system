31335f41d5867606fd5d02427ba4380003d8ec21
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-11-03 12:33:15 +0100

    Create method for get data into histogram based on the column name, column type, and optionally number of ranges

--- src/main/java/pl/edu/pb/swd/Service/DataForChartsService.java
+++ src/main/java/pl/edu/pb/swd/Service/DataForChartsService.java
@@ -1,17 +1,27 @@
 package pl.edu.pb.swd.Service;
 
 import org.springframework.stereotype.Service;
+import pl.edu.pb.swd.Model.Range;
 
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.TreeSet;
+
+import static pl.edu.pb.swd.Service.ReusableOperationsService.convertStringValueToDouble;
 
 @Service
 public class DataForChartsService {
 
     final ReusableOperationsService reusableOperationsService;
 
-    public DataForChartsService(ReusableOperationsService reusableOperationsService) {
+    final TextToNumericService textToNumericService;
+
+    final DiscretizationService discretizationService;
+
+    public DataForChartsService(ReusableOperationsService reusableOperationsService, TextToNumericService textToNumericService, DiscretizationService discretizationService) {
         this.reusableOperationsService = reusableOperationsService;
+        this.textToNumericService = textToNumericService;
+        this.discretizationService = discretizationService;
     }
 
     public LinkedList<LinkedList<String>> getDataTo2dChart(String firstColumnName,
@@ -27,6 +37,68 @@ public class DataForChartsService {
             linkedLists.addAll(Collections.singleton(thirdColumn));
         }
         return linkedLists;
+    }
+
+    public LinkedList<LinkedList<String>> getDataToHistogram(String columnName,
+                                                             boolean discreteVariable,
+                                                             Integer numberOfRanges){
+        LinkedList<String> column = reusableOperationsService.getWholeColumnByIndex(columnName);
+        LinkedList<LinkedList<String>> result = new LinkedList<>();
+        if(discreteVariable){
+            TreeSet<String> uniqueValue = textToNumericService.findUniqueValuesInGivenColumnInAlphabeticalOrder(columnName);
+            LinkedList<String> uniqueValueLinkedList = new LinkedList<>(uniqueValue);
+            LinkedList<String> countUniqueValues = new LinkedList<>();
+            for(String uniqueValue1: uniqueValueLinkedList){
+                int count = 0 ;
+                for(String value: column){
+                    if(value.equals(uniqueValue1)){
+                        count++;
+                    }
+                }
+                countUniqueValues.add(Integer.toString(count));
+            }
+            result.addAll(Collections.singleton(uniqueValueLinkedList));
+            result.addAll(Collections.singleton(countUniqueValues));
+        }
+        else{
+            LinkedList<Double> columnDouble = convertStringValueToDouble(column);
+            LinkedList<Range> rangeLinkedList = discretizationService.createListRange(columnName, numberOfRanges);
+            LinkedList<String> resultRange = new LinkedList<>();
+            LinkedList<String> countValues = new LinkedList<>();
+            int i=0;
+            char beginningOfaClosedSet = '<',
+                    endOfClosedSet = '>',
+                    endOfOpenSet = ')',
+                    betweenExtremesOfSet = ';';
+            for(Range range: rangeLinkedList){
+                String rangeString = "";
+                if(i!=rangeLinkedList.size()-1) {
+                    rangeString = String.valueOf(beginningOfaClosedSet) + range.getLowerValue() + (betweenExtremesOfSet) + range.getUpperValue() + (endOfOpenSet);
+                }
+                else{
+                    rangeString = String.valueOf(beginningOfaClosedSet) + range.getLowerValue() + (betweenExtremesOfSet) + range.getUpperValue() + (endOfClosedSet);
+                }
+                resultRange.add(rangeString);
+                int count = 0 ;
+                for(Double value: columnDouble){
+                    if(i!=rangeLinkedList.size()-1){
+                        if(value >= range.getLowerValue() && value <range.getUpperValue()){
+                            count++;
+                        }
+                    }
+                    else {
+                        if(value >= range.getLowerValue() && value <=range.getUpperValue()){
+                            count++;
+                        }
+                    }
+                }
+                countValues.add(Integer.toString(count));
+                i++;
+            }
+            result.addAll(Collections.singleton(resultRange));
+            result.addAll(Collections.singleton(countValues));
+        }
+        return result;
     }
 
 }
