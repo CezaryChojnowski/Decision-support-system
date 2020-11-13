2808f427b8b858d9d080223bd5f5ad1aab09b512
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-11-13 01:00:30 +0100

    I hate mahalanobis

--- src/main/java/pl/edu/pb/swd/classification/Controller/ClassificationController.java
+++ src/main/java/pl/edu/pb/swd/classification/Controller/ClassificationController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import pl.edu.pb.swd.classification.Service.MetricService;
 import pl.edu.pb.swd.dataOperations.Service.ReadWriteService;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/classification")
@@ -23,26 +21,6 @@ public class ClassificationController {
 
     @RequestMapping(value = "/numericHeaders", method= RequestMethod.GET)
     public List<String> getNumericHeaders(){
-       LinkedList<String> lista1 = metricService.getNumericHeaders();
-       System.out.println(lista1);
-        LinkedList<Integer> lista2 = metricService.getIndexesNumericColumn(lista1);
-        System.out.println(lista2);
-        LinkedList<String> nowalista = new LinkedList<>();
-        nowalista.add("2.3");
-        nowalista.add("2.3");
-        nowalista.add("2.3");
-        nowalista.add("2.3");
-        Map<Integer, Double> integerDoubleMap = metricService.createMapWithIndexAndValueNewObject(lista2, nowalista);
-        System.out.println(integerDoubleMap);
-//        LinkedList<Double> lista3 = metricService.getVectorOfDistance(integerDoubleMap);
-//        System.out.println(lista3);
-        return null;
-    }
-
-    @RequestMapping(value = "/object", method = RequestMethod.POST)
-    public String classifyObject(@RequestBody List<String> newObject,
-                                 @RequestParam Integer numberOfNeighbors,
-                                 @RequestParam Integer metrics){
-        return null;
+       return metricService.getNumericHeaders();
     }
 }
