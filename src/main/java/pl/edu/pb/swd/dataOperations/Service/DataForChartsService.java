d06d533743564d6b4256be5ce2ad2d7066839e0b
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-12-08 18:27:08 +0100

    Sort decision classes set. Required for a fixed color set of specific classes in the client side chart

--- src/main/java/pl/edu/pb/swd/dataOperations/Service/DataForChartsService.java
+++ src/main/java/pl/edu/pb/swd/dataOperations/Service/DataForChartsService.java
@@ -24,26 +24,28 @@ public class DataForChartsService {
     }
 
     public LinkedList<LinkedList<String>> getDataTo2dChart(String firstColumnName,
-                                               String secondColumnName,
-                                               String thirdColumnName){
-        if( thirdColumnName!=null && !thirdColumnName.isEmpty()){
+                                                           String secondColumnName,
+                                                           String thirdColumnName) {
+        if (thirdColumnName != null && !thirdColumnName.isEmpty()) {
             LinkedList<LinkedList<String>> linkedLists = new LinkedList<>();
             LinkedList<LinkedList<String>> data = reusableOperationsService.readWriteService.readDataFromWorkingFile();
             int firstColumnIndex = findColumnIndex(data.getFirst(), firstColumnName);
             int secondColumnIndex = findColumnIndex(data.getFirst(), secondColumnName);
             Integer columnIndex = findColumnIndex(data.getFirst(), thirdColumnName);
             data.remove();
-            Set<String> classTemp = new HashSet<>();
-            for(LinkedList<String> row: data){
-                classTemp.add(row.get(columnIndex));
+            HashSet<String> decisionClasses = new HashSet<>();
+            for (LinkedList<String> row : data) {
+                decisionClasses.add(row.get(columnIndex));
             }
-            Iterator<String> itr = classTemp.iterator();
-            while(itr.hasNext()){
+            TreeSet decisionClassesSet = new TreeSet();
+            decisionClassesSet.addAll(decisionClasses);
+            Iterator<String> itr = decisionClassesSet.iterator();
+            while (itr.hasNext()) {
                 LinkedList<String> firstColumn = new LinkedList<>();
                 LinkedList<String> secondColumn = new LinkedList<>();
                 String temp = itr.next();
-                for(int i=0; i<data.size(); i++){
-                    if(temp.equals(data.get(i).get(columnIndex))){
+                for (int i = 0; i < data.size(); i++) {
+                    if (temp.equals(data.get(i).get(columnIndex))) {
                         firstColumn.add(data.get(i).get(firstColumnIndex));
                         secondColumn.add(data.get(i).get(secondColumnIndex));
                     }
@@ -63,17 +65,17 @@ public class DataForChartsService {
 
     public LinkedList<LinkedList<String>> getDataToHistogram(String columnName,
                                                              boolean discreteVariable,
-                                                             Integer numberOfRanges){
+                                                             Integer numberOfRanges) {
         LinkedList<String> column = reusableOperationsService.getWholeColumnByIndex(columnName);
         LinkedList<LinkedList<String>> result = new LinkedList<>();
-        if(discreteVariable){
+        if (discreteVariable) {
             TreeSet<String> uniqueValue = textToNumericService.findUniqueValuesInGivenColumnInAlphabeticalOrder(columnName);
             LinkedList<String> uniqueValueLinkedList = new LinkedList<>(uniqueValue);
             LinkedList<String> countUniqueValues = new LinkedList<>();
-            for(String uniqueValue1: uniqueValueLinkedList){
-                int count = 0 ;
-                for(String value: column){
-                    if(value.equals(uniqueValue1)){
+            for (String uniqueValue1 : uniqueValueLinkedList) {
+                int count = 0;
+                for (String value : column) {
+                    if (value.equals(uniqueValue1)) {
                         count++;
                     }
                 }
@@ -81,35 +83,32 @@ public class DataForChartsService {
             }
             result.addAll(Collections.singleton(uniqueValueLinkedList));
             result.addAll(Collections.singleton(countUniqueValues));
-        }
-        else{
+        } else {
             LinkedList<Double> columnDouble = convertStringValueToDouble(column);
             LinkedList<Range> rangeLinkedList = discretizationService.createListRange(columnName, numberOfRanges);
             LinkedList<String> resultRange = new LinkedList<>();
             LinkedList<String> countValues = new LinkedList<>();
-            int i=0;
+            int i = 0;
             char beginningOfaClosedSet = '<',
                     endOfClosedSet = '>',
                     endOfOpenSet = ')',
                     betweenExtremesOfSet = ';';
-            for(Range range: rangeLinkedList){
+            for (Range range : rangeLinkedList) {
                 String rangeString = "";
-                if(i!=rangeLinkedList.size()-1) {
+                if (i != rangeLinkedList.size() - 1) {
                     rangeString = String.valueOf(beginningOfaClosedSet) + range.getLowerValue() + (betweenExtremesOfSet) + range.getUpperValue() + (endOfOpenSet);
-                }
-                else{
+                } else {
                     rangeString = String.valueOf(beginningOfaClosedSet) + range.getLowerValue() + (betweenExtremesOfSet) + range.getUpperValue() + (endOfClosedSet);
                 }
                 resultRange.add(rangeString);
-                int count = 0 ;
-                for(Double value: columnDouble){
-                    if(i!=rangeLinkedList.size()-1){
-                        if(value >= range.getLowerValue() && value <range.getUpperValue()){
+                int count = 0;
+                for (Double value : columnDouble) {
+                    if (i != rangeLinkedList.size() - 1) {
+                        if (value >= range.getLowerValue() && value < range.getUpperValue()) {
                             count++;
                         }
-                    }
-                    else {
-                        if(value >= range.getLowerValue() && value <=range.getUpperValue()){
+                    } else {
+                        if (value >= range.getLowerValue() && value <= range.getUpperValue()) {
                             count++;
                         }
                     }
